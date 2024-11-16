f1893943b30874e03a1037503f99e43410a6e69c
spockerman <campos.sandro@gmail.com>
2024-11-16 08:32:18 -0300

    update npm version to fix eslint problem

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,8 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'next',
+    'next/core-web-vitals'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
